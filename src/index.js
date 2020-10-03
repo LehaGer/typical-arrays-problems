395922a3187afe8662aaccfc888a242237756630
LehaGer <leha.ger97@mail.ru>
2020-10-03 23:09:49 +0300

    feat: all functions realised

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array && array.length > 0){
+        let minVal = array[0];
+        array.forEach(element => {
+            if(element < minVal) minVal = element;
+        });
+        return minVal;
+    }else{
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array && array.length > 0){
+        let maxVal = array[0];
+        array.forEach(element => {
+            if(element > maxVal) maxVal = element;
+        });
+        return maxVal;
+    }else{
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array && array.length > 0){
+        let sum = 0;
+        array.forEach(element => {
+            sum += element;
+        });
+        return sum / array.length;
+    }else{
+        return 0;
+    }
 }
